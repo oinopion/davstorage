30cce16fd8365e693fe5a5655926a6a6aa67c182
Tomek Paczkowski <tomek@hauru.eu>
2013-08-20 22:35:20 +0200

    Take default urls from settings

--- davstorage/storage.py
+++ davstorage/storage.py
@@ -1,12 +1,16 @@
 from __future__ import unicode_literals
 import requests
+from django.conf import settings
 from django.core.files import File
 from django.core.files.storage import Storage
 from davstorage.utils import trim_trailing_slash
 
 
 class DavStorage(Storage):
-    def __init__(self, internal_url, external_url):
+    def __init__(self, internal_url=None, external_url=None):
+        if internal_url is None and external_url is None:
+            internal_url = settings.DAVSTORAGE_PRIVATE_URL
+            external_url = settings.DAVSTORAGE_PUBLIC_URL
         self._internal_url = trim_trailing_slash(internal_url)
         self._external_url = trim_trailing_slash(external_url)
 
